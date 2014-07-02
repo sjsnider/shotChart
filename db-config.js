37f85d8e01b5e3c8a39acfaf23fb6e8617f0dc5d
Scott Snider <sjsnider@gmail.com>
2014-07-01 21:13:34 -0700

    environmental variable

--- db-config.js
+++ db-config.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-var url = 'mongodb://sjsnider:minlyhr13@ds030827.mongolab.com:30827/MongoLab-lo';
+var url = process.env.MONCONNECT;
 mongoose.connect(url);
 module.exports = mongoose;
